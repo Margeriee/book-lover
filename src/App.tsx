0b7035b65eeb3a5620337e2cd45e758b9a35ed24
unknown <malgorzata.brzuska@gmail.com>
2023-03-18 23:11:23 +0100

    feat/App-structure-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css'
 import Header from './Header/Header';
 import Main from './Main/Main';
-import Section from './Section/Section';
+import { Routes, Route } from 'react-router-dom'
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Main />
-      <Section />
+      <Routes>/
+        <Route path="/" element={<Main />}/>
+        {/* <Route path="/favorites" element={<Section />}/> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Main/BookCart.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BookCart.css';
+
+const BookCart = () => {
+  return (
+    <div className="bookCart">
+      <img src="./HeaderBackground.jpg" alt=""/>
+      <div className="bottom">
+        <h3 className="title">Title</h3>
+        <p className="description">Description</p>
+      </div>
+    </div>
+  );
+}
+
+export default BookCart;
